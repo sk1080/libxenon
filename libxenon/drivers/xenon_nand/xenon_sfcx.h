bbd16b6b3cc76a06e25b1acd7b451d8d2115dca2
tuxuser <tuxuser@xps.(none)>
2011-11-21 01:26:52 +0800

    xenon_nand: Deleted global buffer "sfcx_page[]" from source/headers newlib: Added additional "return to xell"-offset

--- libxenon/drivers/xenon_nand/xenon_sfcx.h
+++ libxenon/drivers/xenon_nand/xenon_sfcx.h
@@ -65,9 +65,6 @@
 
 #define CONFIG_BLOCKS			0x04			//Number of blocks assigned for config data
 
-static unsigned char sfcx_page[MAX_PAGE_SZ];   //Max known hardware physical page size
-static unsigned char sfcx_block[MAX_BLOCK_SZ]; //Max known hardware physical block size
-
 #define RAW_NAND_64				0x4200000
 
 #define SFCX_INITIALIZED		1
